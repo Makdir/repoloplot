616d3d502a6b264b9b482a6532bbdbf6086f1a96
Makdir <makdir@gmail.com>
2020-12-03 01:31:14 +0200

    Initial commit

---
+++ src/components/DatesRangePicker.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import DatePicker from 'react-datepicker';
+import Row from 'react-bootstrap/Button';
+ 
+import "react-datepicker/dist/react-datepicker.css";
+
+class DatesRangePicker extends React.Component {
+
+  render() {
+    //const [startDate, setStartDate] = useState(new Date());
+    return (
+
+      <Row className = 'p-200'>
+        <DatePicker
+          selected={this.props.startDate}
+          onChange={date => this.props.setStartDate(date)}
+          selectsStart
+          startDate={this.props.startDate}
+          endDate={this.props.endDate}
+        />
+        <b>&nbsp;-&nbsp;</b>
+        <DatePicker
+            selected={this.props.endDate}
+            onChange={date => this.props.setEndDate(date)}
+            selectsEnd
+            startDate={this.props.startDate}
+            endDate={this.props.endDate}
+            minDate={this.props.startDate}
+        />
+      </Row>
+
+    );
+  }
+}
+
+export default DatesRangePicker 
